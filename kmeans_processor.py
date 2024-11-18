15761e1cbdf1d2bf1b0aa0b3bc9fd06b474dc1e2
hans-um <ssoon1234@icloud.com>
2024-11-18 16:30:29 -0600

    Update kmeans_processor.py

--- kmeans_processor.py
+++ kmeans_processor.py
@@ -10,8 +10,8 @@ class KMeansProcessor:
         k_result = {}
         kmeans = KMeans()
 
-        for i, k_df in enumerate(self.sub_dfs.values()):
-            k_df_drop = k_df.drop('current_size', axis=1)
+        for key in self.sub_dfs:
+            k_df_drop = self.sub_dfs[key].drop('current_size', axis=1)
             k_encoded = pd.get_dummies(k_df_drop, columns=['fire_position_on_slope', 'wind_direction', 'fire_type', 'weather_conditions_over_fire'])
 
             param_grid = {'n_clusters': [2, 3, 4, 5, 6]}
@@ -22,7 +22,7 @@ class KMeansProcessor:
                 kmeans = KMeans(n_clusters=n, random_state=42)
                 kmeans.fit(k_encoded)
                 silhouette_avg = silhouette_score(k_encoded, kmeans.labels_)
-                print(f"Group :{i}  n_clusters: {n}, silhouette_score: {silhouette_avg}")
+                # print(f"Group :{key}  n_clusters: {n}, silhouette_score: {silhouette_avg}")
                 if silhouette_avg > silhouette_best:
                     silhouette_best = silhouette_avg
                     best_k = n
@@ -30,14 +30,15 @@ class KMeansProcessor:
             kmeans = KMeans(n_clusters=best_k, random_state=42)
             kmeans.fit(k_encoded)
             labels = kmeans.labels_
-            
+
+            k_df = self.sub_dfs[key]
             k_df['label'] = labels
             k_df['current_size_avg'] = k_df.groupby('label')['current_size'].transform('mean')
             for col in ['temperature', 'wind_speed', 'relative_humidity', 'current_size']:
                 k_df[f'{col}_max'] = k_df.groupby('label')[col].transform('max')
                 k_df[f'{col}_min'] = k_df.groupby('label')[col].transform('min')
             
-            k_df.to_csv(f"kmean_result_{i}.csv", index=False)
-            k_result[i] = k_df
+            # k_df.to_csv(f"kmean_result_{key}.csv", index=False)
+            k_result[key] = k_df
 
         return k_result
